14f0b613d2ac3642075281833b7ceafe7293f417
duramana <duramana.kalumvuati@laposte.net>
2018-02-04 20:06:45 +0100

    Fonction AfficheBits fonctionne très bien

--- fonctions.h
+++ fonctions.h
@@ -2,16 +2,33 @@ typedef struct
 {
     int **bin;
     int *taille;
+    int tailDeTaille;
 }BinaireP;
 
 typedef struct
 {
     int bin[100][100];
     int *taille;
+    int tailDeTaille;
 }BinaireS;
 
+void AfficherBits(BinaireP bits)
+{
+    int i,j;
 
-BinaireP converter(char mot[], int base)
+    for(i = 0; i < bits.tailDeTaille; i++)
+    {
+        for(j = 0; j < bits.taille[i]; j++)
+        {
+            printf("%X ",bits.bin[i][j]);
+        }
+        printf(" ");
+    }
+    printf("\n");
+}
+
+
+BinaireP converterP(char mot[], int base)
 {
     int i=0,j=0;
     int taille;
@@ -21,6 +38,7 @@ BinaireP converter(char mot[], int base)
     BinaireP tabl;
     tabl.bin =(int **)malloc(tailleDeMot*sizeof(int *));
     tabl.taille =(int *)malloc(tailleDeMot*sizeof(int));//Tableau qui vai garder la taille de chaque case
+    tabl.tailDeTaille = tailleDeMot;//Elle garder la taille du tableau de taille
     while(mot[i]!='\0')
     {
         valLettre = mot[i];//Récuperation de la valeur du mot en caractère
--- main.c
+++ main.c
@@ -5,7 +5,15 @@
 
 int main()
 {
-
+    char mot[100];
+    int base =0;
+    printf("Tapez votre mot pour le convertir en Binaire :");
+    scanf("%s",mot);
+    printf("Tapez la base voulu :");
+    scanf("%d",&base);
+    printf("Le mot \"%s\" est écrit de cette manière à la base %d : \n",mot,base);
+    BinaireP motBit= converterP(mot,base);
+    AfficherBits(motBit);
     
     return 0;
 }
